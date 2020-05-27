041985c6c56a297efee85bbd047530bcb2e25774
Ben Buhse <ben@buhses.com>
2020-05-26 23:27:37 -0500

    Update files for archpad 2020.05.26-23:27:37

--- update.sh
+++ update.sh
@@ -1,34 +1,37 @@
 #!/bin/zsh
 
+# Used to determine which directory to copy into
+HOST=$(uname -n)
+
 # Pull branch
 git pull
 
 ##### .config stuf ####
 # Alacritty
-cp -r /home/ben/.config/alacritty .config/
+cp -r /home/ben/.config/alacritty $HOST/.config/
 
 # sway
-cp -r /home/ben/.config/sway .config/
+cp -r /home/ben/.config/sway $HOST/.config/
 
 # waybar
-cp -r /home/ben/.config/waybar .config/
+cp -r /home/ben/.config/waybar $HOST/.config/
+
+# neovim stuff
+cp /home/ben/.config/nvim/init.vim $HOST/.config/nvim/
 
 #### ~ stuff  ####
 # tmux
-cp /home/ben/.tmux.conf .
+cp /home/ben/.tmux.conf $HOST/.
 
 # vim
-cp /home/ben/.vimrc .
+cp /home/ben/.vimrc $HOST/.
 
 # zsh stuff
-cp /home/ben/.zpreztorc .
-cp /home/ben/.zshrc .
-cp /home/ben/.zprofile .
-
-# neovim stuff
-cp /home/ben/.config/nvim/init.vim .config/nvim/
+cp /home/ben/.zpreztorc $HOST/.
+cp /home/ben/.zshrc $HOST/.
+cp /home/ben/.zprofile $HOST.
 
 # Commit and push
 git add -A
-git commit -m "Update files $(date +%Y.%m.%d-%H:%M:%S)"
+git commit -m "Update files for $HOST $(date +%Y.%m.%d-%H:%M:%S)"
 git push
