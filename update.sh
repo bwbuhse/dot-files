c44e1810893cf1706e0e6b1658b58b441c310813
Ben Buhse <ben@buhses.com>
2020-06-12 16:45:58 -0500

    Update files for archtop 2020.06.12-16:45:58

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ben/projects/dot-files/
+cd /home/$USER/projects/dot-files/
 
 # Used to determine which directory to copy into
 HOST=$(uname -n)
@@ -14,59 +14,59 @@ git pull
 
 ##### .config stuff ####
 # Alacritty
-if [ -e /home/ben/.config/alacritty ]; then
-  cp -r /home/ben/.config/alacritty $HOST/.config/
+if [ -e /home/$USER/.config/alacritty ]; then
+  cp -r /home/$USER/.config/alacritty $HOST/.config/
 fi
 
 # sway
-if [ -e /home/ben/.config/sway ]; then
-    cp -r /home/ben/.config/sway $HOST/.config/
+if [ -e /home/$USER/.config/sway ]; then
+    cp -r /home/$USER/.config/sway $HOST/.config/
 fi
 
 # waybar
-if [ -e /home/ben/.config/waybar ]; then
-  cp -r /home/ben/.config/waybar $HOST/.config/
+if [ -e /home/$USER/.config/waybar ]; then
+  cp -r /home/$USER/.config/waybar $HOST/.config/
 fi
 
 # neovim stuff
-if [ -e /home/ben/.config/nvim ]; then
+if [ -e /home/$USER/.config/nvim ]; then
   if [ ! -e $HOST/.config/nvim ]; then
     mkdir $HOST/.config/nvim
   fi
-  cp /home/ben/.config/nvim/init.vim $HOST/.config/nvim/
+  cp /home/$USER/.config/nvim/init.vim $HOST/.config/nvim/
 fi
 
 # coc.nvim
-if [ -e /home/ben/.config/coc ]; then
-  cp -r /home/ben/.config/coc $HOST/.config/coc/
+if [ -e /home/$USER/.config/coc ]; then
+  cp -r /home/$USER/.config/coc $HOST/.config/coc/
 fi
 
 # Polybar
-if [ -e /home/ben/.config/polybar ]; then
+if [ -e /home/$USER/.config/polybar ]; then
   cp -r ~/.config/polybar $HOST/.config    
 fi
 
 # i3
-if [ -e /home/ben/.config/i3 ]; then
+if [ -e /home/$USER/.config/i3 ]; then
   cp -r ~/.config/i3 $HOST/.config
 fi
 
 # picom
-if [ -e /home/ben/.config/picom ]; then
+if [ -e /home/$USER/.config/picom ]; then
   cp -r ~/.config/picom $HOST/.config
 fi
 
 #### ~ stuff  ####
 # tmux
-if [ -e /home/ben/.tmux.conf ]; then
-  cp /home/ben/.tmux.conf $HOST/.
+if [ -e /home/$USER/.tmux.conf ]; then
+  cp /home/$USER/.tmux.conf $HOST/.
 fi
 
 # zsh stuff
-if [ -e /home/ben/.zshrc ]; then
-  cp /home/ben/.zpreztorc $HOST/.
-  cp /home/ben/.zshrc $HOST/.
-  cp /home/ben/.zprofile $HOST/.
+if [ -e /home/$USER/.zshrc ]; then
+  cp /home/$USER/.zpreztorc $HOST/.
+  cp /home/$USER/.zshrc $HOST/.
+  cp /home/$USER/.zprofile $HOST/.
 fi
 
 # Commit and push
@@ -75,7 +75,7 @@ git commit -m "Update files for $HOST $(date +%Y.%m.%d-%H:%M:%S)"
 git push
 
 # Update prezto too!
-if [ -e /home/ben/.zprezto ]; then
+if [ -e /home/$USER/.zprezto ]; then
   (
     cd ~/.zprezto
     git pull
